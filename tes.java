68d159ae191f4acbf3b33d1f7a0a952e7c9d3e63
542241732-33smktelmlg <talitha_aurely_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:58:32 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,8 @@
+public class tes {
+
+    public static void main(String[] args) {
+        System.out.println("Nama saya talitha dari kelas x rpl 7 berasal dari kota ponorogo");
+        String a = "1";
+    }
+    
+}
